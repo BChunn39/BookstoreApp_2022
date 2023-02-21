bc3121d58c27130d02d859276686d617c8748d51
BChunn39 <113707879+BChunn39@users.noreply.github.com>
2023-02-20 20:58:10 -0500

    added VS studio project

---
+++ API_with_EF_2022/Migrations/20230131023724_fishupadte.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_with_EF_2022.Migrations
+{
+    public partial class fishupadte : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
